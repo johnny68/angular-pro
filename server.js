3643533483d5d3fbbeb20db941ec0f50d4c3de7c
Siddhant Ghosh <siddhanta.ghosh.5@gmail.com>
2019-03-06 21:52:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/Royal-Gym'));
+app.use(express.static(__dirname + '/dist/royalgym'));
 
 app.get('/*', function(req,res) {
     
