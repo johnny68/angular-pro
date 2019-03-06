693f7f6de51bffb10819b232ece8ef0b9299d695
Siddhant Ghosh <siddhanta.ghosh.5@gmail.com>
2019-03-07 00:32:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,5 +12,5 @@ app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+'/dist/royalgym/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
+//Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || config.httpPort);
