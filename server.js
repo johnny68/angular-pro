daa5ba2529cad9c94f10a68b9854b03d1404607a
Siddhant Ghosh <siddhanta.ghosh.5@gmail.com>
2019-03-06 21:38:12 +0530

    updates to deploy to heroku 1

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/Royal-Gym'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/Royal-Gym/index.html'));
+res.sendFile(path.join(__dirname+'/dist/royalgym/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
